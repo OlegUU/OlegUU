5a75d95e182adcd31af6d2e5bacf480c1c723805
OlegUU <dreampmatcher@gmail.com>
2024-10-17 21:29:14 +0700

    Add files via upload

---
+++ module_1_6.Словари и распространение.py
@@ -0,0 +1,12 @@
+my_dict = {"Oleg": 1994, 'Город': "Новокузнецк"}
+print(my_dict)
+print(my_dict["Oleg"])
+my_dict = {'имя': 'Oleg', 'год': 1994, 'Город': "Новокузнецк"}
+my_dict.pop("имя")
+print(my_dict)
+my_set={43,43,4.3,4.3,'Hello','Hello'}
+print(my_set)
+my_set.update(['32', True])
+print(my_set)
+my_set.pop()
+print(my_set)
