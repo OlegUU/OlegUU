2ba2041c0750819754a0f0b606d2520a1b23646e
OlegUU <dreampmatcher@gmail.com>
2024-10-17 18:51:35 +0700

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var= (1,2, True, 5.6, 'Hello')
+print(immutable_var)
+# immutable_var [0] = 5 # tuple нельзя присвоить новое значение
+mutable_list = ["apple", "coconut", "banana"]
+mutable_list [0]= "peach"
+print(mutable_list)
+
